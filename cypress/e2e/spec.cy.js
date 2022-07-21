0d6f109fcec30973a25e50795be3022430b76425
Umasowmya <uma.sowmya@zemosolabs.com>
2022-07-21 11:52:14 +0530

    cypress added

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,30 @@
+/// <reference types="Cypress" />
+import '../support/e2e';
+
+context('Perform Test', () => {
+    
+    describe('Perform test', ()=>{
+
+      it('Navigate to freshworks site', () => {
+          cy.visit('https://www.freshworks.com');
+      })
+
+      
+
+
+    it('selecting by index',()=>{                           //traversal 
+
+        cy.visit('https://www.amazon.in')
+
+    
+
+
+    })
+
+
+    
+
+
+})
+
+})
